f6f254eb07d982fb678a46227295ff9d23c22877
lleos-jpg <leonelsechuk2@gmail.com>
2026-03-03 00:51:57 -0300

    db updated (mi octavogesimocomit waos (mi quinto commit))

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,22 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "biblioteca_turnos"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error("Error conectando a MySQL:", err);
-        return;
-    }
-    console.log("Conectado a MySQL ✅");
+  if (err) {
+    console.error("Error conectando a MySQL:", err);
+  } else {
+    console.log("Conectado a MySQL 🚀");
+  }
 });
 
 module.exports = connection;
